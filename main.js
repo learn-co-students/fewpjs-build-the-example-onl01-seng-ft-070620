9e34e5611c149484c498a7244d05fa61c38de84f
JacksonReynolds <jwreynod@gmail.com>
2020-10-10 18:13:11 -0400

    Done.

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", (e) => {
+  const errorModal = document.querySelector("#modal")
+  const likeBtns = document.querySelectorAll(".like")
+
+
+  for (const btn of likeBtns) {
+    const heart = btn.querySelector("span")
+    console.log(heart)
+    btn.addEventListener("click", (e) => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.getAttribute("class") != "activated-heart") {
+            heart.textContent = FULL_HEART
+            heart.setAttribute("class", "activated-heart")
+          } else {
+            heart.textContent = EMPTY_HEART
+            heart.setAttribute("class", "like-glyph")
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          errorModal.setAttribute("class", "")
+          errorModal.innerHTML += `**${error}**`
+          setTimeout(() => {
+            errorModal.setAttribute("class", "hidden")
+            errorModal.innerHTML = "<h2>Error!</h2>"
+          }, 5000)
+        })
+    })
+  }
+
+
+})
+
+
 
 
 
